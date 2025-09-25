065cffd5f45916fdb276f3e7cc62e73805e62ce0
DColeman4268 <Colemdan@sheridancollege.ca>
2025-09-25 08:31:30 -0400

    version 1

---
+++ practice/src/main/java/git/practice/Practice.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package git.practice;
+
+/**
+ *
+ * @author dgrco
+ */
+public class Practice {
+
+    public static void main(String[] args) {
+        System.out.println("Code Version 1");
+        
+        
+        
+    }
+}
